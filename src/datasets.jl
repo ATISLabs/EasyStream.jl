365b02458ccfed305b4a5301da1a2b145aaee152
Filipe Braida <filipebraida@gmail.com>
2020-06-19 18:28:08 -0300

    corrigindo bug

--- src/datasets.jl
+++ src/datasets.jl
@@ -3,6 +3,6 @@ using CSV
 Dataset1CDT() = Dataset1CDT(150, 1)
 
 function Dataset1CDT(initial_size::Int, flux_size::Int)
-    data = CSV.read(path; header = false)
+    data = CSV.read("datasets/1CDT.csv"; header = false)
     return MemoryBuffer(data, initial_size, flux_size)
 end
