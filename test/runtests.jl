e934618e039375e86a51ec8862985c095664778c
Pedro <pedrooconradoo@gmail.com>
2020-08-19 14:47:28 -0300

    Adicionando alguns testes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,33 +1,21 @@
 using EasyStream
 using Test
 
-stream = EasyStream.Dataset1CDT(5)
+@testset "sort and shuffle functionalities" begin
+    df = EasyStream.DataFrame(x = [1, 2, 3, 4, 5, 6], y = [6, 5, 4, 3, 2, 1])
+    
+    cnn = EasyStream.TablesConnector(df, orderBy = :y)
+    for x in df[:,1] 
+        @test EasyStream.next(cnn)[2][1] == x
+    end
 
-filter = EasyStream.FilterModifier([:Column1, :Column1])
+    cnn = EasyStream.TablesConnector(df, orderBy = :x, rev = true)
+    for y in df[:,2] 
+        @test EasyStream.next(cnn)[1][1] == y
+    end
 
-EasyStream.push!(stream, filter)
-
-EasyStream.listen(stream)
-
-
-
-
-using DataFrames
-
-x = stream.connector.rows[1]
-
-
-
-values1 = DataFrame[]
-
-push!(values1, DataFrame(x))
-
-DataFrame(values1)
-
-w = vcat(values1...)
-
-
-w
-
-
-select!(w, :Column1)
+    missing_names = [:c, :d, :e]
+    for name in missing_names 
+        @test_logs (:warn,"A tabela não possui a coluna $name") EasyStream.TablesConnector(df, orderBy = name)
+     end
+end
