258478cb2b7e1332220483a97bfc1f867f7cbe6d
Pedro <pedrooconradoo@gmail.com>
2020-08-22 18:01:23 -0300

    Desmembrando a função de ordenação

--- src/connector.jl
+++ src/connector.jl
@@ -21,19 +21,26 @@ Base.length(conn::TablesConnector) = length(conn.rows)
 
 hasnext(conn::TablesConnector) = conn.state < length(conn)
 
-function TablesConnector(data;
-    orderBy::Symbol = :default,
-    rev::Bool = false, 
-    shuffle::Bool = false)
-
+function TablesConnector(data)
     if !Tables.istable(data)
         @error "the data must be of type Tables"
     end
 
+    return TablesConnector(Tables.rows(data), 0)
+end
+
+function TablesConnector(data, shuffle::Bool)
+
     shuffle == true ? data = data[Random.shuffle(1:size(data,1)), :] : nothing
+
+    return TablesConnector(data)
+end
+
+function TablesConnector(data, orderBy::Symbol; rev::Bool = false)
+
     orderBy != :default && (orderBy in propertynames(data) ? data = sort(data, orderBy, rev = rev) : @warn "A tabela não possui a coluna $orderBy")
 
-    return TablesConnector(Tables.rows(data), 0)
+    return TablesConnector(data)
 end
 
 TablesConnector(filename::String) = TablesConnector(CSV.read(filename; header = false))
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,18 +4,18 @@ using Test
 @testset "sort and shuffle functionalities" begin
     df = EasyStream.DataFrame(x = [1, 2, 3, 4, 5, 6], y = [6, 5, 4, 3, 2, 1])
     
-    cnn = EasyStream.TablesConnector(df, orderBy = :y)
-    for x in df[:,1] 
+    cnn = EasyStream.TablesConnector(df, :y)
+    for x in df[:,1]
         @test EasyStream.next(cnn)[2][1] == x
     end
 
-    cnn = EasyStream.TablesConnector(df, orderBy = :x, rev = true)
-    for y in df[:,2] 
+    cnn = EasyStream.TablesConnector(df, :x, rev = true)
+    for y in df[:,2]
         @test EasyStream.next(cnn)[1][1] == y
     end
 
     missing_names = [:c, :d, :e]
-    for name in missing_names 
-        @test_logs (:warn,"A tabela não possui a coluna $name") EasyStream.TablesConnector(df, orderBy = name)
+    for name in missing_names
+        @test_logs (:warn,"A tabela não possui a coluna $name") EasyStream.TablesConnector(df, name)
      end
 end
