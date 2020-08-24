fe6dfa939be858eae264b2f462ef20e955a77a07
Filipe Braida <filipebraida@gmail.com>
2020-08-24 11:58:02 -0300

    Adicionando test para shuffle

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using EasyStream
 using DataFrames
 using Test
 
-@testset "sort functionalities" begin
+@testset "sort and shuffle functionalities" begin
     df = DataFrame(x = [1, 2, 3, 4, 5, 6], y = [6, 5, 4, 3, 2, 1])
     
     conn = EasyStream.TablesConnector(df, :x)
@@ -21,4 +21,20 @@ using Test
     for name in missing_names
         @test_logs (:warn,"The dataset doesn't have the column $name") EasyStream.TablesConnector(df, name)
      end
+end
+
+@testset "shuffle functionalities" begin
+    df = DataFrame(x = [1:100 ...])
+    
+    conn = EasyStream.TablesConnector(df, shuffle = true)
+    diff_elements = 0
+
+    for x in df[:, :x]
+        batch = EasyStream.next(conn)
+        if batch[1, :x] != x
+            diff_elements += 1
+        end
+    end
+
+    @test diff_elements > 0
 end
