dd9d793271c9a5ce2c0bd1b055f1002e214aaa02
Filipe Braida <filipebraida@gmail.com>
2020-08-24 16:29:03 -0300

    add range type to support inf lopp

--- src/stream.jl
+++ src/stream.jl
@@ -70,3 +70,22 @@ function listen(stream::BatchStream)::DataFrame
 
     return vcat(values...)
 end
+
+struct Range
+    state::Int
+    stream::AbstractStream
+end
+
+function Base.iterate(range::Range, state = 1)
+    if state > range.state
+        return nothing
+    end
+
+    data = listen(range.stream)
+
+    return isempty(data) ? nothing : (data, state+1)
+end
+
+function range(value::Int, stream::AbstractStream)
+    return Range(value, stream)
+end
