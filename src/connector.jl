41809412be004457f9cef5b685ec607b5dc2bdd7
Filipe Braida <filipebraida@gmail.com>
2020-08-24 16:28:48 -0300

    adding GeneratorConnector

--- src/connector.jl
+++ src/connector.jl
@@ -2,24 +2,14 @@ using Tables
 
 abstract type AbstractConnector end
 
+Base.length(conn::AbstractConnector) = Inf
+hasnext(conn::AbstractConnector) = true
+reset!(conn::AbstractConnector) = nothing
+
 mutable struct TablesConnector <: AbstractConnector
     rows
     state::Int
 end
-
-function next(conn::AbstractConnector)
-    if conn.state >= length(conn)
-        return nothing
-    end
-
-    conn.state += 1
-
-    return DataFrame([conn.rows[conn.state]])
-end
-
-Base.length(conn::TablesConnector) = length(conn.rows)
-
-hasnext(conn::TablesConnector) = conn.state < length(conn)
 
 function TablesConnector(data; shuffle::Bool = false)
     if !Tables.istable(data)
@@ -45,4 +35,34 @@ end
 
 TablesConnector(filename::String) = TablesConnector(CSV.read(filename; header = false))
 
+Base.length(conn::TablesConnector) = length(conn.rows)
+hasnext(conn::TablesConnector) = conn.state < length(conn)
+
+function next(conn::TablesConnector)
+    if conn.state >= length(conn)
+        return nothing
+    end
+
+    conn.state += 1
+
+    return DataFrame([conn.rows[conn.state]])
+end
+
 reset!(conn::TablesConnector) = conn.state = 0
+
+mutable struct GeneratorConnector <: AbstractConnector
+    generator::Function
+    args::Dict{Symbol, Any}
+end
+
+function GeneratorConnector(generator::Function;
+                            args...)
+    return GeneratorConnector(generator, args)
+end
+
+function next(conn::GeneratorConnector)
+    total = 100
+    data = conn.generator(;n_samples = total, conn.args...)
+    
+    return DataFrame(data[1 + Int(floor(rand(1,1)[1] .* size(data)[1])), :])
+end
