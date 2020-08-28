a04d6adb134132fa04694c48a3a8b9fd9ed8e85c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-28 22:04:26 +0000

    build based on 02e1e9d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#EasyStream.jl-1","page":"Home","title":"EasyStream.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [EasyStream]","category":"page"}]
+}
