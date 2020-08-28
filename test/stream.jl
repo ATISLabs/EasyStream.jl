c3913058492cf1f0738201d61450d3a58217903d
Filipe Braida <filipebraida@gmail.com>
2020-08-28 16:18:53 -0300

    adding stream tests

--- test/stream.jl
+++ test/stream.jl
@@ -1,42 +1,73 @@
-
 @testset "Stream" begin
-    @testset "Batch" begin 
-        df = EasyStream.DataFrame(x = [1, 2, 3, 4, 5, 6], y = [6, 5, 4, 3, 2, 1])
+    @testset "AbstractStream" begin
+        elements = 10
+        df = DataFrame(x = [1:elements ...], y = [1:elements ...])
         conn = EasyStream.TablesConnector(df)
-        n_rows = size(df, 1)
-        for batch = 1:n_rows
-            EasyStream.reset!(conn)
-            stream = EasyStream.BatchStream(conn, batch = batch)
-
-            i = 1
-            while i <= n_rows
-                index = i + batch -  1
-                (index > n_rows) && (index = n_rows)
-                
-                df_row = df[i:index, :]
-                stream_next = EasyStream.listen(stream)
-
-                for j = 1:size(df_row, 1)
-                    for k = 1:size(df_row, 2)
-                        @test stream_next[j, k] == df_row[j, k]
-                    end
-                end
-                i = index + 1
-            end
-        end
-
-        EasyStream.reset!(conn)
         stream = EasyStream.BatchStream(conn)
-        @test stream.event.time == 0
-
-        i = 1
-        for stream_next in stream
-            df_row = df[i, :]
-            for j=1:size(df, 2)
-                @test stream_next[1, j] == df_row[j]
-            end
-            i += 1 
+        qnt_loops = 0
+        for data in stream
+            qnt_loops = qnt_loops + 1
         end
+        
+        EasyStream.reset!(stream)
+
+        qnt_loops_reset = 0
+        for data in stream
+            qnt_loops_reset = qnt_loops_reset + 1
+        end
+
+        @test qnt_loops == qnt_loops_reset
+    end
+
+    @testset "BatchStream" begin
+        elements = 100
+        df = DataFrame(x = [1:elements ...], y = [1:elements ...])
+        conn = EasyStream.TablesConnector(df)
+
+        batch = 10
+        stream = EasyStream.BatchStream(conn, batch = batch)
+        data = EasyStream.listen(stream)
+        @test size(data)[1] == batch
+        @test size(data)[2] == size(df)[2]
+
+        elements = 5
+        df = DataFrame(x = [1:elements ...], y = [1:elements ...])
+        conn = EasyStream.TablesConnector(df)
+
+        batch = 10
+        stream = EasyStream.BatchStream(conn, batch = batch)
+        data = EasyStream.listen(stream)
+        @test size(data)[1] < batch
+        @test size(data)[2] == size(df)[2]
+        @test size(data)[1] == elements
+
+        elements = 5
+        df = DataFrame(x = [1:elements ...], y = [1:elements ...])
+        conn = EasyStream.TablesConnector(df)
+
+        stream = EasyStream.BatchStream(conn)
+        i = 0
+        for data in stream
+            @test size(data)[1] == 1
+            @test size(data)[2] == size(df)[2]
+            i = i + 1
+        end
+
+        elements = 100
+        df = DataFrame(x = [1:elements ...], y = [1:elements ...])
+        conn = EasyStream.TablesConnector(df)
+
+        batch = 100
+
+        stream = EasyStream.BatchStream(conn, batch = batch)
+        i = 0
+        for data in stream
+            @test size(data)[1] == batch
+            @test size(data)[2] == size(df)[2]
+            i = i + 1
+        end
+
+        @test i == Int(elements ./ batch)
     end
 end
 
